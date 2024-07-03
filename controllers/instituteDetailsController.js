eb182cb39e23c0ba445507898e4afdf30e74a96e
Nishant Phule <nishantphule12@gmail.com>
2024-07-03 19:41:22 +0530

    institute Details update

--- controllers/instituteDetailsController.js
+++ controllers/instituteDetailsController.js
@@ -72,6 +72,34 @@ const instituteDetailsController = {
       }
     );
   },
+  getInstInfo: async (req, res) => {
+    const { code } = req.params;
+    institutesConnection.query(
+      `SELECT dte_inst_code, inst_code,exam_center,dist_center,rac,eqcenter,shift,affil_year,reg_inst,inst_name,trust_address,inst_address,city,inst_dist,nam_princpl,pri_std,pri_ph, pri_cell,pri_email,pri_altemail,pri_off_std,pri_off_ph,pri_fax_std,pri_fax_no,othr_name_per,othr_std,oth_ph,othr_email,oth_mobile,coname,codesig,costd,coph,coeid,comobile,inst_std,inst_ph,inst_fax_std,inst_fax,inst_email,trust_name,trust_std,trust_ph, trust_email, aicte_letter_no, govt_letter_no, aicte_letter_date, govt_letter_date, aictefromdate, aictetodate, modified_on, entry_on, inst_web, dte_letter_no, dte_letter_date, pci_letter_no, pci_letter_date, othr_desig FROM ${INST_SHORT_INFO} WHERE inst_code=${code}`,
+      (error, results, fields) => {
+        if (error) {
+          console.error("Error querying MySQL:", error);
+          res.status(500).send("Error fetching data from MySQL");
+          return;
+        }
+        res.json(results); // Return data as JSON
+      }
+    );
+  },
+  getDistrictName: async (req, res) => {
+    const { code } = req.params;
+    institutesConnection.query(
+      `SELECT * FROM ${DISTRICT_MASTER} where code=${code}`,
+      (error, results, fields) => {
+        if (error) {
+          console.error("Error querying MySQL:", error);
+          res.status(500).send("Error fetching data from MySQL");
+          return;
+        }
+        res.json(results); // Return data as JSON
+      }
+    );
+  },
 };
 
 module.exports = instituteDetailsController;
--- routes/instituteDetailsRoutes.js
+++ routes/instituteDetailsRoutes.js
@@ -2,6 +2,8 @@ const {
   getDetails,
   getDteCode,
   getDDdetails,
+  getInstInfo,
+  getDistrictName,
 } = require("../controllers/instituteDetailsController");
 
 const instituteDetailsRouter = require("express").Router();
@@ -11,5 +13,9 @@ instituteDetailsRouter.get("/instituteInfo/:id", getDetails);
 instituteDetailsRouter.get("/getDteCode/:code", getDteCode);
 
 instituteDetailsRouter.get("/getDDdetails/:code", getDDdetails);
+
+instituteDetailsRouter.get("/getInstInfo/:code", getInstInfo);
+
+instituteDetailsRouter.get("/getDistrictName/:code", getDistrictName);
 
 module.exports = instituteDetailsRouter;
